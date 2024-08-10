6348983f3027c6d496c9ea3fcbabfa76c4f92fbf
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-08-10 11:00:53 -0300

    feat: add booking item

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -7,7 +7,9 @@ const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
-        <Image alt="JN Barber" src="/logo.png" height={18} width={120} />
+        <a href="/">
+          <Image alt="JN Barber" src="/logo.png" height={18} width={120} />
+        </a>
         <Button size="icon" variant="outline">
           <MenuIcon></MenuIcon>
         </Button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import { SearchIcon } from "lucide-react"
+import { RocketIcon, SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { Alert, AlertDescription, AlertTitle } from "./_components/ui/alert"
 
 export default function Home() {
   return (
@@ -13,12 +17,15 @@ export default function Home() {
         <h2 className="text-xl font-bold">Olá, Jhon</h2>
         <p>Sexta-feira, 09 de agosto</p>
 
+        {/* BUSCA */}
         <div className="mt-5 flex items-center gap-2">
           <Input placeholder="Faça sua busca..."></Input>
           <Button>
             <SearchIcon />
           </Button>
         </div>
+
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[180px] w-full">
           <Image
             alt="Agende agora nos melhores com JN Barber"
@@ -27,6 +34,30 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex items-center">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
+                </Avatar>
+                <p className="text-sm">Barbearia JN Barber</p>
+              </div>
+            </div>
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">25</p>
+              <p className="text-sm">10:15</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
