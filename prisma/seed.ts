43fbc650920c21e6beab75c1c0b09876120d8e0d
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-08-10 20:16:18 -0300

    feat: update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -112,7 +112,7 @@ async function seedDatabase() {
           name,
           address,
           imageUrl: imageUrl,
-          phones: ["(11) 99999-9999", "(11) 99999-9999"],
+          phones: ["(85) 9698-3127", "(85) 9698-3127"],
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac augue ullamcorper, pharetra orci mollis, auctor tellus. Phasellus pharetra erat ac libero efficitur tempus. Donec pretium convallis iaculis. Etiam eu felis sollicitudin, cursus mi vitae, iaculis magna. Nam non erat neque. In hac habitasse platea dictumst. Pellentesque molestie accumsan tellus id laoreet.",
         },
