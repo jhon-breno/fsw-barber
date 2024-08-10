918154166dcaef4b76876c7e62dd8d18c514218c
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-08-09 22:15:23 -0300

    chore: update shadcn theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "FSW Barber",
   description: "Aplicação para barbearias",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className="dark">
       <body className={inter.className}>{children}</body>
       <link rel="icon" href="./icon.png" />
     </html>
-  );
+  )
 }
