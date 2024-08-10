61d568082ed3059e3309c7cb6aa60978f24f9058
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-08-10 12:43:36 -0300

    feat: add BarbershopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,12 @@ import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { Alert, AlertDescription, AlertTitle } from "./_components/ui/alert"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item"
 
-export default function Home() {
+const Home = async () => {
+  // chamar o banco de dados
+  const barbershops = await db.barbershop.findMany({})
   return (
     <div>
       {/* HEAD */}
@@ -36,14 +40,16 @@ export default function Home() {
         </div>
 
         {/* AGENDAMENTO */}
-
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* Esquerda */}
             <div className="flex flex-col gap-2 py-5 pl-5">
               <Badge className="w-fit">Confirmado</Badge>
               <h3 className="font-semibold">Corte de cabelo</h3>
-              <div className="flex items-center">
+              <div className="flex items-center gap-1">
                 <Avatar className="h-6 w-6">
                   <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
                 </Avatar>
@@ -58,7 +64,18 @@ export default function Home() {
             </div>
           </CardContent>
         </Card>
+
+        <h2 className="mt-6text-xs mb-3 font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   )
 }
+
+export default Home
