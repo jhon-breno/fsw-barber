50c90114866c9351df4895430c23dd3720894a7d
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-08-09 23:10:20 -0300

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button>Meu primeiro Botão</Button>
+  return (
+    <div>
+      {/* HEAD */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Jhon</h2>
+        <p>Sexta-feira, 09 de agosto</p>
+
+        <div className="mt-5 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..."></Input>
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[180px] w-full">
+          <Image
+            alt="Agende agora nos melhores com JN Barber"
+            src="/Banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
