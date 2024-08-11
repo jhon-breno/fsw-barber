ebccafc47390afc50282fc7f6ac74c43f6a3dba9
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-08-11 13:23:17 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -5,6 +5,14 @@ import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_contants/search"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -13,14 +21,39 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu Login.</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conect-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-4 font-bold">
+              <Image
+                src="/google-icon.svg"
+                height={24}
+                width={24}
+                alt="Fazer login com o google"
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
         <div className="ml-3 flex flex-col">
           <span className="font-bold">Jhon Breno</span>
           <span className="text-xs text-gray-500">jhonbreno@gmail.com</span>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
