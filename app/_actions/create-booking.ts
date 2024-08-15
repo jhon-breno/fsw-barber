88534d7454526fc0f64cd37e5fefba738465f7ec
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-08-15 17:05:59 -0300

    feat: ensure only authenticated user can book

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -1,17 +1,23 @@
 "use server"
 
-import { revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache"
 import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
 
 interface CreateBookingParams {
-    userId: string;
-    serviceId: string;
-    date: Date
+  serviceId: string
+  date: Date
 }
 
 export const createBooking = async (params: CreateBookingParams) => {
-    await db.booking.create({
-        data: params,
-    })
-    revalidatePath("/barbershops/[id]")
+  const user = await getServerSession(authOptions)
+  if (!user) {
+    throw new Error("Usuário não autenticado")
+  }
+  await db.booking.create({
+    data: { ...params, userId: (user.user as any).id },
+  })
+  revalidatePath("/barbershops/[id]")
+  revalidatePath("/bookings")
 }
