bfd6656d8ddaa3b88af850c935ca08f0d41bf961
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-08-16 18:25:05 -0300

    chore: add NEXT_AUTH_SECRET env variable

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -5,20 +5,21 @@ import GoogleProvider from "next-auth/providers/google"
 import { Adapter } from "next-auth/adapters"
 
 export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(db) as Adapter,
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   callbacks: {
-    async session({session, user}) {
+    async session({ session, user }) {
       session.user = {
         ...session.user,
         id: user.id,
       } as any
       return session
-    }
-  }
+    },
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
 }
